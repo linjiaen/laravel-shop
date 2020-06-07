c4b9f358db41579d2c526cf142b7413d5f45f1c3
vagrant <vagrant@homestead>
2020-06-07 14:48:39 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 Auth::routes(['verify' => true]);
 Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['middleware' =>'auth','verified'],function(){
-    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+Route::group(['middleware' => 'auth', 'verified'], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-    Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-    Route::delete('user_addresses/{user_address}','UserAddressesController@destroy')->name('user_addresses.destroy');
+    Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 });
