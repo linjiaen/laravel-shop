260279a09de32d186146a240f05aa8b128386f29
vagrant <vagrant@homestead>
2020-06-07 16:21:34 +0000

    用户界面 - 商品收藏

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Exceptions\InvalidRequestException;
+
 class ProductsController extends Controller
 {
     public function index(Request $request)
@@ -50,12 +51,39 @@ class ProductsController extends Controller
         ]);
     }
 
-    public function show(Product $product , Request $request)
+    public function show(Product $product, Request $request)
     {
         if (!$product->on_sale) {
             throw new InvalidRequestException('商品已经下架');
         }
+        $favored = false;
+        // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
+        if ($user = $request->user()) {
+            // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
+            // boolval() 函数用于把值转为布尔值
+            $favored = boolval($user->favoriteProducts()->find($product->id));
+        }
 
-        return view('products.show',['product' => $product]);
+        return view('products.show', ['product' => $product, 'favored' => $favored]);
+    }
+    public function favor(Product $product, Request $request)
+    {
+        $user = $request->user();
+
+        if ($user->favoriteProducts()->find($product->id)) {
+            return [];
+        }
+
+        $user->favoriteProducts()->attach($product->id);
+
+        return [];
+    }
+
+    public function disfavor(Product $product, Request $request)
+    {
+        $user = $request->user();
+        $user->favoriteProducts()->detach($product->id);
+
+        return [];
     }
 }
