503737c5259a66b5a11289da5198ec1c05dea7ed
vagrant <vagrant@homestead>
2020-06-08 15:16:11 +0000

    封装业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,41 +6,34 @@ use App\Http\Requests\AddCartRequest;
 use Illuminate\Http\Request;
 use App\Models\CartItem;
 use App\Models\ProductSku;
+use App\Services\CartService;
+
 class CartController extends Controller
 {
+    protected $cartService;
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
     public function add(AddCartRequest $request)
     {
         $user = $request->user();
         $skuId = $request->input('sku_id');
         $amount  = $request->input('amount');
-        if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-
-            // 如果存在则直接叠加商品数量
-            $cart->update([
-                'amount' => $cart->amount + $amount,
-            ]);
-        } else {
-
-            // 否则创建一个新的购物车记录
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
-
+        $this->cartService->add($skuId,$amount);
         return [];
     }
 
     public function index(Request $request)
     {
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         $address = $request->user()->addresses()->orderBy('last_used_at','desc')->get();
         return view('cart.index', ['cartItems' => $cartItems ,'addresses' => $address]);
     }
 
     public function remove(productSku $sku,Request $request)
     {
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+       $this->cartService->remove($sku->id);
         return [];
     }
 }
